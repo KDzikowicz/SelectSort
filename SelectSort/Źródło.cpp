daefe6b980a351135df0d60748e5fcf70bc74382
Karol Dzikowicz <karol.dzikowicz@szkola.salezjanie.com>
2022-03-10 11:08:57 +0100

    .

--- SelectSort/Źródło.cpp
+++ SelectSort/Źródło.cpp
@@ -2,23 +2,29 @@
 #include<algorithm>
 #include<ctime>
 using namespace std;
-void selectsort(int n, int a[]) {
-	int k{}, pom{}, j{};
-	do {
-		j++;
+void SelectSort(int n, int a[]) {
+	int pom{}, j{};
+	for (int k{};k < n; k++) {
 		for (int l = j + 1;l < n;l++) {
 			if (a[j] > a[l]) {
 				pom = a[j];
 				a[j] = a[l];
 				a[l] = pom;
-				k++;
 			} 
 		}
-	} while (k != (n - 1));
+		j++;
+	}
+
 	cout << "Po sortowaniu\n";
 	for (int x = 0;x < n;x++) {
 		cout << a[x] << endl;
 	}
+}
+void InsertSort(int n, int a[]) {
+	int pom{}, j{}, l{};
+
+
+
 }
 int main() {
 	srand(time(NULL));
@@ -28,6 +34,6 @@ int main() {
 		cout << tab[i] << endl;
 	}
 	int n_tab = sizeof(tab) / sizeof(int);
-	selectsort(n_tab, tab);
+	SelectSort(n_tab, tab);
 	return 0;
 }
